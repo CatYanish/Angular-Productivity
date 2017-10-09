15e002ca7cdf040df5a39039dd750865ff6106c0
Cat Yanish <catyanish@gmail.com>
2017-10-08 21:07:07 -0500

    completed post route to add goal to db

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('UserController', function(UserService) {
+myApp.controller('UserController', function(UserService, $http) {
   console.log('UserController created');
   var vm = this;
   vm.userService = UserService;
@@ -10,8 +10,11 @@ myApp.controller('UserController', function(UserService) {
   }
 
 
-  vm.logGoal = function() {
+  vm.addGoal = function() {
     console.log("new goal", vm.newGoal);
-  }
+    $http.post('/goal/add', vm.newGoal).then(function(response) {
+      console.log('this is the response', response);
+    })
+  } //end addGoal function
 
-});
+}); //end controller
